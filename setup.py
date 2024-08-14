8997e8bf776e808163eff742abfe035f73ca0274
Ali Ayad <16190163+ayadagain@users.noreply.github.com>
2024-08-14 04:07:51 +0300

    test: ci

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
-    license="BSD-3",
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
